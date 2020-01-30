972327766d5ffbb01778f35feacfd3d96696aead
Erayus <raymondhieutran@gmail.com>
2020-01-31 02:12:32 +1100

    [Fix] SchoolSelection UX

--- src/containers/SchoolSelection/SchoolSelection.tsx
+++ src/containers/SchoolSelection/SchoolSelection.tsx
@@ -7,28 +7,30 @@ import { ISchool } from '../../models/school';
 import { RootStoreContext } from '../../stores/rootStore';
 
 const SchoolSelection: React.FC<RouteComponentProps> =  ({history}) => {
+    const initialSchoolObj = {id:"", name: "none", logoUrl: ""};
     const schoolSelectionBox = useRef<HTMLSelectElement>(null);
-    const [selectedSchool, setSelectedSchool] = useState<ISchool>({id:"", name: "none", logoUrl: ""});
+    const [selectedSchool, setSelectedSchool] = useState<ISchool>(initialSchoolObj);
     const rootStore = useContext(RootStoreContext);
 
-    const { schools,loadSchools} = rootStore.schoolStore;
+    const { schools} = rootStore.schoolStore;
 
     useEffect(() => {
-        loadSchools();
-        
         const selectedSchoolFromLS:any = localStorage.getItem('selectedSchool');
-        if (selectedSchoolFromLS !== "none") {
-            setSelectedSchool(schools.filter(school => school.name === selectedSchoolFromLS)[0]);
-        };
-        schoolSelectionBox.current!.value = selectedSchoolFromLS;
         
+        selectedSchoolFromLS === "none" || !selectedSchoolFromLS ? setSelectedSchool(initialSchoolObj)
+        : setSelectedSchool(schools.filter(school => school.name === selectedSchoolFromLS)[0]);
+    
+        if (schoolSelectionBox.current) {
+            schoolSelectionBox.current!.value = selectedSchoolFromLS;
+        }
     }, [rootStore, schools.length])
 
 
     const onSelectSchool = (event: ChangeEvent<HTMLSelectElement>) => {
-        console.log(event.target.value);
+        const schoolName = event.target.value;
         localStorage.setItem('selectedSchool', event.target.value );
-        setSelectedSchool(schools.filter(school => school.name === event.target.value)[0]);
+        schoolName !== "none" ? setSelectedSchool(schools.filter(school => school.name === event.target.value)[0]) 
+        : setSelectedSchool(initialSchoolObj) 
     }
     const startSurvey = () => {
         history.push('/survey/' + selectedSchool.id);
@@ -39,14 +41,21 @@ const SchoolSelection: React.FC<RouteComponentProps> =  ({history}) => {
                 <h1 style={{fontWeight: "bolder"}}>INSTRUCTORUS</h1>
                 <hr></hr>
                 <h5>{selectedSchool?.name !== "none" ? "You have selected" : "Please select a school" }</h5>
-                <select onChange={onSelectSchool}  ref={schoolSelectionBox} className="browser-default custom-select">
-                    <option value="none">Select</option>
-                    {schools.map(school => (
-                         <option key={school.name} value={school.name}>{school.name}</option>
-                    ))}
-                   
-                </select>
-                {selectedSchool?.name !== "none" ? <MDBBtn className="mt-4" color="success" onClick={startSurvey}> Start Survey </MDBBtn> : null}
+                {
+                    schools.length > 0 ? (
+                        <React.Fragment>
+                        <select onChange={onSelectSchool}  ref={schoolSelectionBox} className="browser-default custom-select">
+                            <option value="none">Select</option>
+                            {schools.map(school => (
+                                <option key={school.name} value={school.name}>{school.name}</option>
+                            ))}
+                        </select>
+                        </React.Fragment>
+                    ) : <div>Loading...</div>
+                }
+                {selectedSchool?.name !== "none"  && schools.length > 0  ? <MDBBtn className="mt-4" color="success" onClick={startSurvey}> Start Survey </MDBBtn> : null }
+
+                
             </div>
     )
     
